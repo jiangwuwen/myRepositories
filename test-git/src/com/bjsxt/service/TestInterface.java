c34075ed45a926e70de47c2a413e8af1d8339f68
jiangwuwen <503256870@qq.com>
2019-05-15 07:02:30 +0800

    111

--- test-git/src/com/bjsxt/service/TestInterface.java
+++ test-git/src/com/bjsxt/service/TestInterface.java
@@ -5,4 +5,11 @@ public interface TestInterface {
     void m1();
     void m2();
     void m3();
+    void m4();
+    void m5();
+    void m6();
+    void m7();
+    void m8();
+    void m9();
+    void m10();
 }
