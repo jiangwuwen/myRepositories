d8237d2e7f432eaaef8d262a05a1ca3db1f14758
jiangwuwen <503256870@qq.com>
2019-05-15 21:31:09 +0800

    commit 1

--- test-git/src/com/bjsxt/service/TestInterface.java
+++ test-git/src/com/bjsxt/service/TestInterface.java
@@ -1,15 +1,7 @@
 package com.bjsxt.service;
 
 public interface TestInterface {
-    void m();
     void m1();
     void m2();
     void m3();
-    void m4();
-    void m5();
-    void m6();
-    void m7();
-    void m8();
-    void m9();
-    void m10();
 }
